c925ff9ec64d9495d02dbbfc2f9d02dbd0bfd9d2
LycanCross <bay.to.me2@gmail.com>
2017-11-23 16:07:13 -0700

    Listado clase 3

---
+++ listado/detalleusuarios.js
@@ -0,0 +1,27 @@
+
+const {BrowserWindow}=require('electron').remote
+const app=require('electron').app;
+const $ = require('jquery');
+
+//Constantes para llamar al PE DE EFE
+const ipc.require('electron').ipcRender;
+const botonPDF=document.getElementByID('btnPDF');
+
+botonPDF.addEventListener('click',function(event){
+	botonPDF.style.display="none"
+	ipc.send('print-to-pdf')
+});
+
+var nombre=require('electron').remote.getGlobal('infoUsuarios').nombre;
+var genero=require('electron').remote.getGlobal('infoUsuarios').genero;
+var foto=require('electron').remote.getGlobal('infoUsuarios').foto;
+var direccion=require('electron').remote.getGlobal('infoUsuarios').direccion;
+var telefono=require('electron').remote.getGlobal('infoUsuarios').telefono;
+$("#idNombre").html(nombre);
+$("#idGenero").html(genero);
+$("#idDireccion").html(direccion);
+$("#idTelefono").html(telefono);
+$("#idFoto").attr("src",foto);
+
+
+
---
+++ listado/main.js
@@ -0,0 +1,75 @@
+
+// const app=require('electron').app;
+// const BrowserWindow=require('electron').BrowserWindow;
+const {app,BrowserWindow} = require('electron');
+const path = require('path'); //Muestra la ruta del archivo
+const url = require('url'); //Carga una página
+
+//Constantes para PDF
+const electron = require('electron');
+const fs = require('fs');	//Sistema de archivos
+//Acceso al sistema operativo
+const os = require('os');
+//Para declarar una función remota
+const ipc = electron.ipcMain;
+//Acceso a la terminal
+const shell = electron.shell;
+
+
+// ECMASCRIPT = 6
+let PantallaPrincipal;
+//Objeto global para compartir datos
+//entre pantallas 
+global.infoUsuarios = {
+	nombre: '',
+	genero: '',
+	foto: '',
+	direccion: '',
+	telefono: ''
+}
+
+function muestraPantallaPrincipal(){
+	PantallaPrincipal = new BrowserWindow({width:320,height:425});
+	PantallaPrincipal.loadURL(url.format({
+		pathname: path.join(__dirname,'index.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+	PantallaPrincipal.webContents.openDevTools();
+	PantallaPrincipal.show();
+}
+
+//Evento para PE DE EFE
+ipc.on('print-to-pdf',function(event){
+	const pdfPath = path.join(os.tmpdir(),'print.pdf')
+	const win = BrowserWindow.fromWebContents(event.sender)
+	win.webContents.printToPDF({},function(error,data){
+		if(error) throw error
+
+		fs.writeFile(pdfPath,data,function(error){
+			if(error){
+				throw error
+			}
+
+			shell.openExternal('file://'+pdfPath)
+			win.close()
+
+		})
+
+	})
+});
+
+app.on('ready',muestraPantallaPrincipal)
+
+
+
+
+
+
+
+
+
+
+
+
+
